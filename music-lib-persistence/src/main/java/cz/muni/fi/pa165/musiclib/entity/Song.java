618f33f974adf6393aa669eb77e688ceaaeb022d
xstefank <mjolnir122@gmail.com>
2015-10-24 15:27:45 +0200

    maven project setup, persistence module

---
+++ music-lib-persistence/src/main/java/cz/muni/fi/pa165/musiclib/entity/Song.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.musiclib.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author xstefank
+ * @version 10/24/15
+ */
+@Entity
+public class Song {
+
+}
