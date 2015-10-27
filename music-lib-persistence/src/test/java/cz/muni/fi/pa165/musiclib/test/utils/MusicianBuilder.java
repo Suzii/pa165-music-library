2881dad5e8eb07626197a367f91a653c576fe8e7
Suzii <z.dankovcikova@gmail.com>
2015-10-27 23:46:50 +0100

    Created utils for testing - builder classes

---
+++ music-lib-persistence/src/test/java/cz/muni/fi/pa165/musiclib/test/utils/MusicianBuilder.java
@@ -0,0 +1,63 @@
+package cz.muni.fi.pa165.musiclib.test.utils;
+
+import cz.muni.fi.pa165.musiclib.entity.Musician;
+import cz.muni.fi.pa165.musiclib.entity.Song;
+import cz.muni.fi.pa165.musiclib.enums.Sex;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Zuzana Dankovcikova
+ */
+public class MusicianBuilder {
+
+        private String artistName;
+        private List<Song> songs = new ArrayList<>();
+        private Sex sex;
+        private Date dateOfBirth;
+        
+        public MusicianBuilder artistName(String artistName) {
+            this.artistName = artistName;
+            return this;
+        }
+        
+        public MusicianBuilder songs(List<Song> songs) {
+            Collections.copy(this.songs, songs);
+            return this;
+        }
+
+        public MusicianBuilder sex(Sex sex) {
+            this.sex = sex;
+            return this;
+        }
+
+        public MusicianBuilder dateOfBirth(Date dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public Musician build() {
+            Musician m = new Musician();
+            m.setArtistName(artistName);
+            m.setSex(sex);
+            m.setDateOfBirth(dateOfBirth);
+            
+            List<Song> copy = new ArrayList<>();
+            Collections.copy(copy, this.songs);
+            m.setSongs(copy);
+            
+            clear();
+            
+            return m;
+        }
+        
+        private void clear() {
+            this.artistName = null;
+            this.dateOfBirth = null;
+            this.sex = null;
+            this.songs = null;
+        }
+    }
---
+++ music-lib-persistence/src/test/java/cz/muni/fi/pa165/musiclib/test/utils/SongBuilder.java
@@ -0,0 +1,80 @@
+package cz.muni.fi.pa165.musiclib.test.utils;
+
+import cz.muni.fi.pa165.musiclib.entity.*;
+
+/**
+ *
+ * @author Zuzana Dankovcikova
+ */
+public class SongBuilder {
+    
+    private String title;
+    private String commentary;
+    private int positionInAlbum;    
+    private double bitrate;
+    private Album album;
+    private Musician musician;
+    private Genre genre;
+    
+    public SongBuilder title(String title) {
+        this.title = title;
+        return this;
+    }
+    
+    public SongBuilder commentary(String commentary) {
+        this.title = title;
+        return this;
+    }
+    
+    public SongBuilder positionInAlbum(int positionInAlbum) {
+        this.positionInAlbum = positionInAlbum;
+        return this;
+    }
+    
+    public SongBuilder bitrate(double bitrate) {
+        this.bitrate = bitrate;
+        return this;
+    }
+    
+    public SongBuilder album(Album album) {
+        this.album = album;
+        return this;
+    }
+    
+    public SongBuilder musician(Musician musician) {
+        this.musician = musician;
+        return this;
+    }
+    
+    public SongBuilder genre(Genre genre) {
+        this.genre = genre;
+        return this;
+    }
+    
+    public Song build() {
+        Song s = new Song();
+        
+        s.setTitle(title);
+        s.setCommentary(commentary);
+        s.setPositionInAlbum(positionInAlbum);
+        s.setBitrate(bitrate);
+        s.setAlbum(album);
+        s.setMusician(musician);
+        s.setGenre(genre);
+        
+        clear();
+        
+        return s;
+    }
+    
+    private void clear() {
+        this.title = null;
+        this.commentary = null;
+        this.bitrate = 0;
+        this.positionInAlbum = 0;
+        this.musician = null;
+        this.genre = null;
+        this.album = null;
+    }
+
+}
