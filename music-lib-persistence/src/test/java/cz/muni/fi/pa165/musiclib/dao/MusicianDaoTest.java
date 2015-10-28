406da71f624a3ca688dc76508c1f50a77eaeff95
Suzii <z.dankovcikova@gmail.com>
2015-10-28 10:55:05 +0100

    MusicianDaoTest partial implementation

--- music-lib-persistence/src/test/java/cz/muni/fi/pa165/musiclib/dao/MusicianDaoTest.java
+++ music-lib-persistence/src/test/java/cz/muni/fi/pa165/musiclib/dao/MusicianDaoTest.java
@@ -2,9 +2,14 @@ package cz.muni.fi.pa165.musiclib.dao;
 
 import cz.muni.fi.pa165.musiclib.PersistenceSampleApplicationContext;
 import cz.muni.fi.pa165.musiclib.entity.*;
+import cz.muni.fi.pa165.musiclib.enums.Sex;
 import cz.muni.fi.pa165.musiclib.test.utils.MusicianBuilder;
 import cz.muni.fi.pa165.musiclib.test.utils.SongBuilder;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -23,6 +28,9 @@ import org.testng.annotations.Test;
 @Transactional
 public class MusicianDaoTest extends AbstractTestNGSpringContextTests {
     
+    @PersistenceContext
+    public EntityManager em;
+    
     @Inject
     public SongDao songDao;
     
@@ -40,8 +48,8 @@ public class MusicianDaoTest extends AbstractTestNGSpringContextTests {
 
     @BeforeMethod
     private void init() {
-        musician1 = musicianBuilder.artistName("Bruno Mars").build();
-        musician2 = musicianBuilder.artistName("Adelle").build();
+        musician1 = musicianBuilder.artistName("Bruno Mars").sex(Sex.Male).build();
+        musician2 = musicianBuilder.artistName("Adelle").sex(Sex.Female).build();
         
         song1A = songBuilder.title("Uptown funk").build();
         song1B = songBuilder.title("Locked out of heaven").build();
@@ -51,19 +59,66 @@ public class MusicianDaoTest extends AbstractTestNGSpringContextTests {
     }
     
     @Test
-    public void createTest() {
-//        musicianDao.create(musician1);        
-//        Musician m = musicianDao.findById(musician1.getId());        
-//        Assert.assertEquals(musician1, m);
+    public void findByIdTest() {
+        musicianDao.create(musician1);
+        Assert.assertEquals(musicianDao.findById(musician1.getId()), musician1);
     }
     
     @Test
-    public void findByIdTest() {
-    
+    public void findByIdNonexistingTest() {
+        Assert.assertNull(musicianDao.findById(new Long(1)));
     }
     
     @Test
     public void findAllTest() {
-    
+        musicianDao.create(musician1);
+        musicianDao.create(musician2);
+        Assert.assertEquals(musicianDao.findAll().size(), 2);
     }
+    
+    @Test
+    public void createTest() {
+        musicianDao.create(musician1);        
+        Assert.assertNotNull(musician1.getId());
+        Musician m = musicianDao.findById(musician1.getId());        
+        Assert.assertEquals(musician1, m);
+        Assert.assertEquals(m.getArtistName(), musician1.getArtistName());
+        Assert.assertEquals(m.getSex(), musician1.getSex());
+        Assert.assertEquals(m.getDateOfBirth(), musician1.getDateOfBirth());
+    }
+    
+    @Test(expectedExceptions=PersistenceException.class)
+    public void createWithSameArtistNamesTest() {
+        Musician m1 = musicianBuilder.artistName("Dupe").sex(Sex.Female).build();
+        Musician m2 = musicianBuilder.artistName("Dupe").sex(Sex.Male).build();
+        musicianDao.create(m1);
+        musicianDao.create(m2);
+    }
+    
+    @Test(expectedExceptions=ConstraintViolationException.class)
+    public void createWithArtistNameNullTest() {
+        Musician m1 = musicianBuilder.sex(Sex.Female).build();
+        musicianDao.create(m1);
+    }
+    
+    @Test(expectedExceptions=ConstraintViolationException.class)
+    public void createWithSexNullTest() {
+        Musician m1 = musicianBuilder.artistName("Madonna").build();;
+        musicianDao.create(m1);
+    }
+    
+    @Test
+    public void createWithSongs() {
+        songDao.create(song1A);
+        songDao.create(song1B);
+        songDao.create(song2A);
+        
+        musicianDao.create(musician1);
+        
+        song1A.setMusician(musician1);
+        song1B.setMusician(musician1);
+        
+        Assert.assertEquals(musicianDao.findById(musician1.getId()).getSongs().size(), 2);
+    }
+    
 }
