cf393167b8a73ed3b8c01c480b3c9f3d063212cd
Suzii <z.dankovcikova@gmail.com>
2015-11-16 10:07:31 +0100

    BeanMappingService added

---
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/BeanMappingService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.musiclib.service;
+
+import java.util.Collection;
+import java.util.List;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Service for mapping beans.
+ * 
+ * @author zdank
+ * @version 16/11/2015
+ */
+public interface BeanMappingService {
+    
+    public <T> List<T> mappTo(Collection<?> source, Class<T> mapToClass);
+    
+    public <T> T mapTo(Object source, Class<T> mapToClass);
+    
+    public Mapper getMapper();
+}
---
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/BeanMappingServiceImpl.java
@@ -0,0 +1,41 @@
+package cz.muni.fi.pa165.musiclib.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author zdank
+ * @version 16/11/2015
+ */
+@Service
+public class BeanMappingServiceImpl implements BeanMappingService {
+
+    @Autowired
+    private Mapper mapper;
+    
+    @Override
+    public <T> List<T> mappTo(Collection<?> source, Class<T> mapToClass) {
+        List<T> result = new ArrayList<>();
+        for(Object o : source){
+            result.add(mapTo(o, mapToClass));
+        }
+        
+        return result;
+    }
+
+    @Override
+    public <T> T mapTo(Object source, Class<T> mapToClass) {
+        return mapper.map(source, mapToClass);
+    }
+
+    @Override
+    public Mapper getMapper() {
+        return mapper;
+    }
+    
+}
