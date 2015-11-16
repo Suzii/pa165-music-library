6999b631c7221ed6e998e3d86107fd13734b5bee
Suzii <z.dankovcikova@gmail.com>
2015-11-16 10:51:51 +0100

    SongService basic methods+javadoc added

--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/SongService.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/SongService.java
@@ -1,9 +1,79 @@
 package cz.muni.fi.pa165.musiclib.service;
 
+import cz.muni.fi.pa165.musiclib.entity.Album;
+import cz.muni.fi.pa165.musiclib.entity.Genre;
+import cz.muni.fi.pa165.musiclib.entity.Musician;
+import cz.muni.fi.pa165.musiclib.entity.Song;
+import java.util.List;
+
 /**
- *
+ * Song service that provides all business logic for song entities. 
+ * 
  * @author zdank
+ * @version 16/11/2015
  */
 public interface SongService {
-    
+    /**
+     * Creates new song.
+     *
+     * @param song entity to be created
+     */
+    void create(Song song);
+
+    /**
+     * Updates song.
+     *
+     * @param song entity to be updated
+     * @return updated song entity
+     */
+    Song update(Song song);
+
+    /**
+     * Returns the song entity attached to the given id.
+     *
+     * @param id id of the song entity to be returned
+     * @return the song entity with given id
+     */
+    Song findById(Long id);
+
+    /**
+     * Returns all Songs.
+     *
+     * @return list of all song entities
+     */
+    List<Song> findAll();
+
+    /**
+     * Returns all songs for the given album
+     *
+     * @param album album to which songs belong
+     * @return list of Song entities in the given album
+     */
+    List<Song> findByAlbum(Album album);
+
+    /**
+     * Return all songs for the given musician
+     *
+     * @param musician musician to whom song belong
+     * @return all Song entities for the given musician
+     * @throws IllegalArgumentException if musician is null
+     */
+    List<Song> findByMusician(Musician musician);
+
+    /**
+     * Returns all songs for the given genre
+     *
+     * @param genre genre to which songs belong
+     * @return all Song entities for the given genre
+     * @throws IllegalArgumentException if the genre is null
+     */
+    List<Song> findByGenre(Genre genre);
+
+    /**
+     * Removes the Song entity from persistence context
+     *
+     * @param song song to be removed     
+     * @throws IllegalArgumentException if passed song is null or is not stored in DB 
+     */
+    void remove(Song song);
 }
--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/SongServiceImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/SongServiceImpl.java
@@ -1,9 +1,60 @@
 package cz.muni.fi.pa165.musiclib.service;
 
+import cz.muni.fi.pa165.musiclib.dao.SongDao;
+import cz.muni.fi.pa165.musiclib.entity.Album;
+import cz.muni.fi.pa165.musiclib.entity.Genre;
+import cz.muni.fi.pa165.musiclib.entity.Musician;
+import cz.muni.fi.pa165.musiclib.entity.Song;
+import java.util.List;
+import org.dozer.inject.Inject;
+
 /**
  *
  * @author zdank
  */
 public class SongServiceImpl implements SongService {
+
+    @Inject
+    private SongDao songDao;
+    
+    @Override
+    public void create(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Song update(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Song findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Song> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Song> findByAlbum(Album album) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Song> findByMusician(Musician musician) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Song> findByGenre(Genre genre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
