1bbb15187933f36ea5a736c5bc969b06c7aad335
Suzii <z.dankovcikova@gmail.com>
2015-11-16 10:56:18 +0100

    AlbumService basic methods+javadoc added

--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/AlbumService.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/AlbumService.java
@@ -1,9 +1,59 @@
 package cz.muni.fi.pa165.musiclib.service;
 
+import cz.muni.fi.pa165.musiclib.entity.Album;
+import java.util.List;
+
 /**
- *
+ * Album service that provides all business logic for album entities. 
+ * 
  * @author zdank
+ * @version 16/11/2015
  */
 public interface AlbumService {
     
+    /**
+     * Created new album.
+     * 
+     * @param album entity to be created
+     */
+    void create(Album album);
+    
+    /**
+     * Updates album.
+     *
+     * @param album entity to be updated
+     * @return updated album entity
+     */
+    Album update(Album album);
+    
+    /**
+     * Removes album.
+     * 
+     * @param album entity to be removed
+     * @throws IllegalArgumentException if passed album is null or is not stored in DB
+     */
+    void remove(Album album) throws IllegalArgumentException;
+    
+    /**
+     * Get album entity by unique ID.
+     * 
+     * @param id id of album entity
+     * @return album entity with given id, null if no album with this id exists
+     */
+    Album findById(Long id);
+    
+    /**
+     * Returns all albums with given title
+     * 
+     * @param title title of albums to look for
+     * @return list of all album entities with given title, empty if no such album exists
+     */
+    List<Album> findByTitle(String title);
+    
+    /**
+     * Returns all albums.
+     * 
+     * @return list of all albums
+     */
+    List<Album> findAll();
 }
--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/AlbumServiceImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/service/AlbumServiceImpl.java
@@ -1,9 +1,47 @@
 package cz.muni.fi.pa165.musiclib.service;
 
+import cz.muni.fi.pa165.musiclib.dao.AlbumDao;
+import cz.muni.fi.pa165.musiclib.entity.Album;
+import java.util.List;
+import javax.inject.Inject;
+
 /**
  *
  * @author zdank
  */
 public class AlbumServiceImpl implements AlbumService {
+
+    @Inject
+    private AlbumDao albumDao;
+    
+    @Override
+    public void create(Album album) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Album update(Album album) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Album album) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Album findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Album> findByTitle(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Album> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
