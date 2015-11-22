1006afed7aeacdbc9389258dbea9386d1bf0ddd5
Suzii <z.dankovcikova@gmail.com>
2015-11-22 18:44:16 +0100

    SongFacade implementation

--- music-lib-api/src/main/java/cz/muni/fi/pa165/musiclib/facade/SongFacade.java
+++ music-lib-api/src/main/java/cz/muni/fi/pa165/musiclib/facade/SongFacade.java
@@ -1,7 +1,62 @@
 package cz.muni.fi.pa165.musiclib.facade;
 
+import cz.muni.fi.pa165.musiclib.dto.SongAddYoutubeLinkDTO;
+import cz.muni.fi.pa165.musiclib.dto.SongDTO;
+import java.util.List;
+
 /**
- * @author
+ * @author zdankovc
+ * @version 22/11/2015
  */
 public interface SongFacade {
+    /**
+     * Creates new song.
+     *
+     * @param song entity to be created
+     * @return id of newly created song
+     */
+    Long create(SongDTO song);
+    
+    void addYoutubeLink(SongAddYoutubeLinkDTO addYoutubeLinkDTO);
+
+    /**
+     * Returns the song entity attached to the given id.
+     *
+     * @param id id of the song entity to be returned
+     * @return the song entity with given id
+     */
+    SongDTO findById(Long id);
+
+    /**
+     * Returns all Songs.
+     *
+     * @return list of all song entities
+     */
+    List<SongDTO> findAll();
+
+    /**
+     * Returns all songs for the given album
+     *
+     * @param albumId id of album to which songs belong
+     * @return list of Song entities in the given album
+     */
+    List<SongDTO> findByAlbum(Long albumId);
+
+    /**
+     * Return all songs for the given musician
+     *
+     * @param musicianId id of musician to whom song belong
+     * @return all Song entities for the given musician
+     * @throws IllegalArgumentException if musician is null
+     */
+    List<SongDTO> findByMusician(Long musicianId);
+
+    /**
+     * Returns all songs for the given genre
+     *
+     * @param genreId id of genre to which songs belong
+     * @return all Song entities for the given genre
+     * @throws IllegalArgumentException if the genre is null
+     */
+    List<SongDTO> findByGenre(Long genreId);
 }
--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/SongFacadeImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/SongFacadeImpl.java
@@ -1,7 +1,89 @@
 package cz.muni.fi.pa165.musiclib.facade;
 
+import cz.muni.fi.pa165.musiclib.dto.SongAddYoutubeLinkDTO;
+import cz.muni.fi.pa165.musiclib.dto.SongDTO;
+import cz.muni.fi.pa165.musiclib.entity.Song;
+import cz.muni.fi.pa165.musiclib.exception.MusicLibServiceException;
+import cz.muni.fi.pa165.musiclib.service.AlbumService;
+import cz.muni.fi.pa165.musiclib.service.BeanMappingService;
+import cz.muni.fi.pa165.musiclib.service.GenreService;
+import cz.muni.fi.pa165.musiclib.service.MusicianService;
+import cz.muni.fi.pa165.musiclib.service.SongService;
+import java.util.List;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
 /**
- * @author Martin Stefanko (mstefank@redhat.com)
+ * @author zdankovc
+ * @version 22/11/2015
  */
+@Service
+@Transactional
 public class SongFacadeImpl implements SongFacade {
+
+    @Inject
+    private BeanMappingService beanMappingService;
+    
+    @Inject
+    private SongService songService; 
+    
+    @Inject
+    private AlbumService albumService;
+    
+    @Inject
+    private GenreService genreService;
+    
+    @Inject
+    private MusicianService musicianService; 
+    
+    @Override
+    public Long create(SongDTO song) {
+        songService.create(beanMappingService.mapTo(song, Song.class));
+        return song.getId();
+    }    
+    
+    @Override
+    public void addYoutubeLink(SongAddYoutubeLinkDTO addYoutubeLinkDTO) {
+        if(addYoutubeLinkDTO.getYoutubeLink() == null){
+            throw new MusicLibServiceException("Cannot add empty youtube link");
+        }
+        if(!addYoutubeLinkDTO.getYoutubeLink().startsWith("https://www.youtube.com")){
+            throw new MusicLibServiceException("Only youtube links are supported."
+                    + " Wrong link format: " + addYoutubeLinkDTO.getYoutubeLink());
+        }
+        Song song = songService.findById(addYoutubeLinkDTO.getSongId());
+        song.setYoutubeLink(addYoutubeLinkDTO.getYoutubeLink());
+    }
+
+    @Override
+    public SongDTO findById(Long id) {
+        return beanMappingService.mapTo(songService.findById(id), SongDTO.class);
+    }
+
+    @Override
+    public List<SongDTO> findAll() {
+        return beanMappingService.mapTo(songService.findAll(), SongDTO.class);
+    }
+
+    @Override
+    public List<SongDTO> findByAlbum(Long albumId) {
+        return beanMappingService.mapTo(
+                songService.findByAlbum(albumService.findById(albumId)), 
+                SongDTO.class);
+    }
+
+    @Override
+    public List<SongDTO> findByMusician(Long musicianId) {
+        return beanMappingService.mapTo(
+                songService.findByMusician(musicianService.findById(musicianId)), 
+                SongDTO.class);
+    }
+
+    @Override
+    public List<SongDTO> findByGenre(Long genreId) {
+        return beanMappingService.mapTo(
+                songService.findByGenre(genreService.findById(genreId)), 
+                SongDTO.class);
+    }
 }
