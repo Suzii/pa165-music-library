91a456836904b9d11ea6766ae28a5af42564f7c3
xstefank <mjolnir122@gmail.com>
2015-11-24 18:30:41 +0100

    blank implementation of MusicFacadeImpl

--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/MusicianFacadeImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/MusicianFacadeImpl.java
@@ -1,8 +1,51 @@
 package cz.muni.fi.pa165.musiclib.facade;
 
+import cz.muni.fi.pa165.musiclib.dto.MusicianDTO;
+
+import java.util.List;
+
 /**
  * @author Milan
  */
 public class MusicianFacadeImpl implements MusicianFacade {
-    
+
+    @Override
+    public Long createMusician(MusicianDTO m) {
+        return null;
+    }
+
+    @Override
+    public void addSong(Long musicianId, Long songId) {
+
+    }
+
+    @Override
+    public void removeSong(Long musicianId, Long songId) {
+
+    }
+
+    @Override
+    public void changeArtistName(String artistName) {
+
+    }
+
+    @Override
+    public void deleteMusician(Long musicianId) {
+
+    }
+
+    @Override
+    public List<MusicianDTO> getAllMusicians() {
+        return null;
+    }
+
+    @Override
+    public MusicianDTO getMusicianById(Long musicianId) {
+        return null;
+    }
+
+    @Override
+    public List<MusicianDTO> getMusicianByArtistName(String artistName) {
+        return null;
+    }
 }
