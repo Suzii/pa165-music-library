c1fd68d970e1bf7e3809fad30ccee3f372916988
Suzii <z.dankovcikova@gmail.com>
2015-11-26 18:36:38 +0100

    AlbumServiceTest initial commit - mocking works

---
+++ music-lib-service/src/test/java/cz/muni/fi/pa165/musiclib/service/AlbumServiceTest.java
@@ -0,0 +1,118 @@
+package cz.muni.fi.pa165.musiclib.service;
+
+import cz.muni.fi.pa165.musiclib.config.ServiceConfiguration;
+import cz.muni.fi.pa165.musiclib.dao.AlbumDao;
+import cz.muni.fi.pa165.musiclib.dao.SongDao;
+import cz.muni.fi.pa165.musiclib.entity.Album;
+import cz.muni.fi.pa165.musiclib.entity.Genre;
+import cz.muni.fi.pa165.musiclib.entity.Musician;
+import cz.muni.fi.pa165.musiclib.entity.Song;
+import cz.muni.fi.pa165.musiclib.enums.Sex;
+import cz.muni.fi.pa165.musiclib.utils.AlbumBuilder;
+import cz.muni.fi.pa165.musiclib.utils.GenreBuilder;
+import cz.muni.fi.pa165.musiclib.utils.MusicianBuilder;
+import cz.muni.fi.pa165.musiclib.utils.SongBuilder;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author zdank
+ * @version 26/11/2015
+ */
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class AlbumServiceTest extends AbstractTestNGSpringContextTests {
+
+    @Mock
+    public SongDao songDao;
+
+    @Mock
+    public AlbumDao albumDao;
+    
+    @InjectMocks
+    public AlbumService albumService = new AlbumServiceImpl();
+    
+    AlbumBuilder albumBuilder = new AlbumBuilder();
+    SongBuilder songBuilder = new SongBuilder();
+    MusicianBuilder musicianBuilder = new MusicianBuilder();
+    GenreBuilder genreBuilder = new GenreBuilder();
+
+    private Musician musician1;
+    private Musician musician2;
+    private Song song1A;
+    private Song song1B;
+    private Song song2A;
+    private Song song2B;
+    private Genre genre;
+    private Album album1;
+    private Album album2;
+    
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @BeforeMethod
+    private void init() {      
+      
+        musician1 = musicianBuilder.artistName("Bruno Mars").sex(Sex.MALE).build();
+        musician2 = musicianBuilder.artistName("Adelle").sex(Sex.FEMALE).build();
+
+        song1A = songBuilder.title("Uptown funk").build();
+        song1B = songBuilder.title("Locked out of heaven").build();
+        song2A = songBuilder.title("Someone like you").build();
+        song2B = songBuilder.title("Rolling in the deep").build();
+
+        genre = genreBuilder.id(1l).title("Pop").build();
+        album1 = albumBuilder.id(1l).title("Hooligans").build();
+        album2 = albumBuilder.id(2l).title("MDN").build();
+
+        song1A.setAlbum(album1);
+        song1B.setAlbum(album1);
+        song2A.setAlbum(album2);
+        song2B.setAlbum(album2);
+
+        song1A.setGenre(genre);
+        song1B.setGenre(genre);
+        song2A.setGenre(genre);
+        song2B.setGenre(genre);
+    }
+
+    @Test
+    public void testClassInitializationTest() {
+        assertNotNull(albumDao);
+        assertNotNull(songDao);
+        assertNotNull(albumService);
+    }
+    
+    @Test
+    public void findByIdTest() {
+        logger.info("Find album by id test...");
+        
+        when(albumService.findById(1l)).thenReturn(album1);
+        Album actual = albumService.findById(1l);
+        
+        assertNotNull(actual);
+        assertDeepEquals(actual, album1);
+    }
+    
+    private void assertDeepEquals(Album album01, Album album02) {
+        assertEquals(album01.getId(), album02.getId());
+        assertEquals(album01.getTitle(), album02.getTitle());
+        assertEquals(album01.getCommentary(), album02.getCommentary());
+        assertEquals(album01.getDateOfRelease(), album02.getDateOfRelease());
+        assertEquals(album01.getAlbumArt(), album02.getAlbumArt());
+        assertEquals(album01.getAlbumArtMimeType(), album02.getAlbumArtMimeType());
+        assertEquals(album01.getSongs(), album02.getSongs());
+    }
+}
