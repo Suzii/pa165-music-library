8df8cfc759afdda786a9265c9f9cd59f0e25fb11
Suzii <z.dankovcikova@gmail.com>
2015-11-26 20:59:03 +0100

    AlbumServiceTest first bulk of test methods

--- music-lib-service/src/test/java/cz/muni/fi/pa165/musiclib/service/AlbumServiceTest.java
+++ music-lib-service/src/test/java/cz/muni/fi/pa165/musiclib/service/AlbumServiceTest.java
@@ -8,19 +8,31 @@ import cz.muni.fi.pa165.musiclib.entity.Genre;
 import cz.muni.fi.pa165.musiclib.entity.Musician;
 import cz.muni.fi.pa165.musiclib.entity.Song;
 import cz.muni.fi.pa165.musiclib.enums.Sex;
+import cz.muni.fi.pa165.musiclib.exception.MusicLibDataAccessException;
 import cz.muni.fi.pa165.musiclib.utils.AlbumBuilder;
 import cz.muni.fi.pa165.musiclib.utils.GenreBuilder;
 import cz.muni.fi.pa165.musiclib.utils.MusicianBuilder;
+import cz.muni.fi.pa165.musiclib.utils.SetIdHelper;
 import cz.muni.fi.pa165.musiclib.utils.SongBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.hibernate.service.spi.ServiceException;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doAnswer;
 import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.when;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -38,10 +50,10 @@ public class AlbumServiceTest extends AbstractTestNGSpringContextTests {
 
     @Mock
     public AlbumDao albumDao;
-    
+
     @InjectMocks
     public AlbumService albumService = new AlbumServiceImpl();
-    
+
     AlbumBuilder albumBuilder = new AlbumBuilder();
     SongBuilder songBuilder = new SongBuilder();
     MusicianBuilder musicianBuilder = new MusicianBuilder();
@@ -56,15 +68,15 @@ public class AlbumServiceTest extends AbstractTestNGSpringContextTests {
     private Genre genre;
     private Album album1;
     private Album album2;
-    
+
     @BeforeClass
     public void setup() throws ServiceException {
         MockitoAnnotations.initMocks(this);
     }
-    
+
     @BeforeMethod
-    private void init() {      
-      
+    public void init() {
+
         musician1 = musicianBuilder.artistName("Bruno Mars").sex(Sex.MALE).build();
         musician2 = musicianBuilder.artistName("Adelle").sex(Sex.FEMALE).build();
 
@@ -88,22 +100,135 @@ public class AlbumServiceTest extends AbstractTestNGSpringContextTests {
         song2B.setGenre(genre);
     }
 
+    @BeforeMethod
+    public void initMocksBehaviour() {
+        when(albumDao.findById(1l)).thenReturn(album1);
+        when(albumDao.findById(2l)).thenReturn(album2);
+        when(albumDao.findById(0l)).thenReturn(null);
+        
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object argument = invocation.getArguments()[0];
+                if(argument == null) {
+                    throw new IllegalArgumentException();
+                }
+                
+                Album album = (Album) argument;
+                if(album.getId() != null){
+                    throw new IllegalArgumentException();
+                }
+                
+                SetIdHelper.setId(album, 42l);                
+                return null;
+            }
+        }).when(albumDao).create(any(Album.class));
+    }
+    
+
+
     @Test
     public void testClassInitializationTest() {
         assertNotNull(albumDao);
         assertNotNull(songDao);
         assertNotNull(albumService);
     }
+
+    @Test
+    public void createTest() {
+        Album newAlbum = albumBuilder.id(null).title("Fresh picks").build();
+        
+        albumService.create(newAlbum);
+        
+        assertNotNull(newAlbum);
+        assertNotNull(newAlbum.getId());
+    }
     
+    @Test(expectedExceptions = MusicLibDataAccessException.class)
+    public void createIdAlreadySetTest() {
+        Album newAlbum = albumBuilder.id(1l).title("Fresh picks").build();
+        
+        albumService.create(newAlbum);
+        
+    }
+
+    @Test
+    public void updateTest() {
+
+    }
+
+    @Test
+    public void removeTest() {
+
+    }
+
+    @Test
+    public void addSongTest() {
+
+    }
+
+    @Test
+    public void removeSongTest() {
+
+    }
+
     @Test
     public void findByIdTest() {
-        when(albumDao.findById(1l)).thenReturn(album1);
         Album actual = albumService.findById(1l);
-        
+
         assertNotNull(actual);
         assertDeepEquals(actual, album1);
     }
-    
+
+    @Test
+    public void findByNonexistingIdReturnsNullTest() {
+        Album actual = albumService.findById(3l);
+
+        assertNull(actual);
+    }
+
+    @Test
+    public void findByTitleTest() {
+        List<Album> expected = Arrays.asList(album1);
+        when(albumDao.findByTitle("Hooligans")).thenReturn(expected);
+        List<Album> actual = albumService.findByTitle("Hooligans");
+
+        assertNotNull(actual);
+        assertEquals(actual, expected);
+        assertDeepEquals(actual.get(0), album1);
+    }
+
+    @Test
+    public void findByNonExistingTitleTest() {
+        List<Album> expected = new ArrayList<>();
+        when(albumDao.findByTitle("Moonshine jungle")).thenReturn(expected);
+        List<Album> actual = albumService.findByTitle("Moonshine jungle");
+
+        assertNotNull(actual);
+        assertEquals(actual.size(), 0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void findAllTest() {
+        when(albumDao.findAll()).thenReturn(Arrays.asList(album1, album2));
+        List<Album> actual = albumService.findAll();
+
+        assertNotNull(actual);
+        assertEquals(actual.size(), 2);
+        assertTrue(actual.contains(album1));
+        assertTrue(actual.contains(album2));
+    }
+
+    @Test
+    public void findAllEpmtyTest() {
+        when(albumDao.findAll()).thenReturn(new ArrayList<Album>());
+        List<Album> actual = albumService.findAll();
+
+        assertNotNull(actual);
+        assertEquals(actual.size(), 0);
+    }
+
     private void assertDeepEquals(Album album01, Album album02) {
         assertEquals(album01.getId(), album02.getId());
         assertEquals(album01.getTitle(), album02.getTitle());
@@ -112,5 +237,21 @@ public class AlbumServiceTest extends AbstractTestNGSpringContextTests {
         assertEquals(album01.getAlbumArt(), album02.getAlbumArt());
         assertEquals(album01.getAlbumArtMimeType(), album02.getAlbumArtMimeType());
         assertEquals(album01.getSongs(), album02.getSongs());
+    }
+    
+    private Album cloneAlbum(Album album, Long id){
+        return albumBuilder.id(id) //set new id
+                        .title(album.getTitle())
+                        .commentary(album.getCommentary())
+                        .dateOfRelease(album.getDateOfRelease())
+                        .albumArt(album.getAlbumArt())
+                        .albumArtMimeType(null)
+                        .albumArt(album.getAlbumArt()
+                        ).songs(album.getSongs())
+                        .build();
+    }
+    
+    private Album cloneAlbum(Album album){
+        return cloneAlbum(album, album.getId());
     }
 }
