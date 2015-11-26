1b2eaf3a707575142ddef4a993164cc283a64973
Suzii <z.dankovcikova@gmail.com>
2015-11-27 00:00:02 +0100

    AlbumFacadeTest: facade tests initial commit

---
+++ music-lib-service/src/test/java/cz/muni/fi/pa165/musiclib/facede/AlbumFacadeTest.java
@@ -0,0 +1,64 @@
+package cz.muni.fi.pa165.musiclib.facede;
+
+import cz.muni.fi.pa165.musiclib.config.ServiceConfiguration;
+import cz.muni.fi.pa165.musiclib.facade.AlbumFacade;
+import cz.muni.fi.pa165.musiclib.facade.AlbumFacadeImpl;
+import cz.muni.fi.pa165.musiclib.service.AlbumService;
+import cz.muni.fi.pa165.musiclib.service.BeanMappingService;
+import cz.muni.fi.pa165.musiclib.service.SongService;
+import javax.inject.Inject;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import static org.testng.Assert.assertNotNull;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author zdank
+ * @version 26/11/2015
+ */
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class AlbumFacadeTest extends AbstractTestNGSpringContextTests {
+    
+    @Mock
+    private AlbumService albumService;
+
+    @Mock
+    private SongService songService;
+
+    @Inject
+    private BeanMappingService beanMappingService;
+    
+    @InjectMocks
+    private AlbumFacade albumFacade = new AlbumFacadeImpl();
+    
+    
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testClassInitializationTest() {
+        assertNotNull(albumService);
+        assertNotNull(songService);
+        assertNotNull(albumFacade);
+    }
+    
+     @BeforeMethod
+    public void initMocksBehaviour() {
+        // findById
+        when(albumService.findById(1l)).thenReturn(null);
+        when(albumService.findById(2l)).thenReturn(null);
+        when(albumService.findById(0l)).thenReturn(null);
+
+    }
+
+}
