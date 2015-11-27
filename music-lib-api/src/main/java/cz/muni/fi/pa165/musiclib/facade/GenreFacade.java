f4d3b01fcbd205963259bcef3625b636c3ed8b82
DiDo <malyborky@gmail.com>
2015-11-27 17:32:59 +0100

    GenreFacade Documentation

--- music-lib-api/src/main/java/cz/muni/fi/pa165/musiclib/facade/GenreFacade.java
+++ music-lib-api/src/main/java/cz/muni/fi/pa165/musiclib/facade/GenreFacade.java
@@ -8,15 +8,49 @@ import java.util.List;
  */
 public interface GenreFacade {
 
+    /**
+    * Creates new genre.
+    *
+    * @param genre entity to be created
+    * @return id of created genre
+    */    
     Long create(GenreDTO genre);
 
+    /**
+     * Returns all genres.
+     *
+     * @return list of all genre entities
+     */
     List<GenreDTO> getAllGenres();
 
+    /**
+     * Returns genre with given id
+     * 
+     * @param id of searched genre
+     * @return genre with given id
+     * @throws IllegalArgumentException if musician is null
+     */
     GenreDTO getGenreById(Long id);
 
+    /**
+     * Returns list of genres with the given title
+     *
+     * @param title of searched genre
+     * @return list of genres with given title
+     */
     List<GenreDTO> getGenreByTitle(String title);
 
+    /**
+     * Changes title of genre
+     * 
+     * @param genre to have title changed
+     */
     void changeTitle(GenreDTO genre);
 
+    /**
+     * Deletes genre 
+     * 
+     * @param genreId
+     */
     void deleteGenre(Long genreId);
 }
