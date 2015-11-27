a0b7ef9e4fc620021d1330aafa4a6ff035694ce2
xstefank <mjolnir122@gmail.com>
2015-11-27 16:28:24 +0100

    UserServiceImpl excaption handling; GenreFacadeTest skeleton

---
+++ music-lib-service/src/test/java/cz/muni/fi/pa165/musiclib/facade/GenreFacadeTest.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.musiclib.facade;
+
+import cz.muni.fi.pa165.musiclib.config.ServiceConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author xstefank (422697@mail.muni.cz)
+ * @version 11/27/15
+ */
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class GenreFacadeTest {
+}
