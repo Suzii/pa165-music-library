3917af65e7544bcba0fd10109e557369a6ea7c6c
xstefank <mjolnir122@gmail.com>
2015-12-05 16:16:13 +0100

    MVC - fix packages typo

--- music-lib-spring-mvc/src/main/java/cz/muni/fi/pa165/musiclib/config/MyStartInitializer.java
+++ music-lib-spring-mvc/src/main/java/cz/muni/fi/pa165/musiclib/config/MyStartInitializer.java
@@ -1,4 +1,4 @@
-package cd.muni.fi.pa165.musiclib.config;
+package cz.muni.fi.pa165.musiclib.config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
