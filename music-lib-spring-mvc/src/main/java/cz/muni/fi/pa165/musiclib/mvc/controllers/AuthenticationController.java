4e866dbff5b121c50a5e88c5e8feb8402c29f3ed
xstefank <mjolnir122@gmail.com>
2015-12-09 23:01:41 +0100

    added AuthenticationController

---
+++ music-lib-spring-mvc/src/main/java/cz/muni/fi/pa165/musiclib/mvc/controllers/AuthenticationController.java
@@ -0,0 +1,60 @@
+package cz.muni.fi.pa165.musiclib.mvc.controllers;
+
+import cz.muni.fi.pa165.musiclib.dto.UserAuthenticationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+
+/**
+ * @author xstefank (422697@mail.muni.cz)
+ * @version 12/9/15
+ */
+@Controller
+public class AuthenticationController {
+
+    final static Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("user", new UserAuthenticationDTO());
+        return "user/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(
+            //DO NOT CHANGE the order of first two parameters
+            @Valid @ModelAttribute("user") UserAuthenticationDTO formBean,
+            BindingResult bindingResult,
+            Model model,
+            @RequestParam(defaultValue = "/home") String redirectTo,
+            RedirectAttributes redirectAttributes,
+            UriComponentsBuilder uriComponentsBuilder) {
+
+        log.debug("create(formBean={})", formBean);
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError ge : bindingResult.getGlobalErrors()) {
+                log.trace("ObjectError: {}", ge);
+            }
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                model.addAttribute(fe.getField() + "_error", true);
+                log.trace("FieldError: {}", fe);
+            }
+            return "user/login";
+        }
+
+        return "redirect:" + redirectTo;
+    }
+}
