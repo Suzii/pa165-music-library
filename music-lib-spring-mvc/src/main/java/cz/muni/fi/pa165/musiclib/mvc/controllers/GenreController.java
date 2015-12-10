1a4d3fd0878ee4d6851c2b44a7c6be47c9ea9c50
DiDo <malyborky@gmail.com>
2015-12-10 11:47:44 +0100

    Genre controller update

--- music-lib-spring-mvc/src/main/java/cz/muni/fi/pa165/musiclib/mvc/controllers/GenreController.java
+++ music-lib-spring-mvc/src/main/java/cz/muni/fi/pa165/musiclib/mvc/controllers/GenreController.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.musiclib.mvc.controllers;
 
+import cz.muni.fi.pa165.musiclib.dto.GenreDTO;
 import cz.muni.fi.pa165.musiclib.facade.GenreFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import javax.inject.Inject;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * 
@@ -26,10 +33,46 @@ public class GenreController {
     @Inject
     private GenreFacade genreFacade;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getGenres(Model model) {
+    @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.GET)
+    public String index(Model model) {
         log.debug("getGenres()");
         model.addAttribute("genres", genreFacade.getAllGenres());
-        return "genre/list";
+        return "genre/index";
+    }
+    
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create(Model model){
+        log.debug("create genre()");
+        model.addAttribute("genreCreate", new GenreDTO());
+        return "genre/create";
+    }
+    
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(
+            @Valid @ModelAttribute("genreCreate") GenreDTO genreFormBean,
+            BindingResult bindingResult,
+            Model model,
+            RedirectAttributes redirectAttributes,
+            UriComponentsBuilder uriComponentsBuilder) {
+        
+        log.debug("create genre(formBean={})", genreFormBean);
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError globalError : bindingResult.getGlobalErrors()) {
+                log.error("ObjectError: {}", globalError);
+            }
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                model.addAttribute(fieldError.getField() + "_error", true);
+                log.error("FieldError: {}", fieldError);
+            }
+
+            return "/genre/create";
+        }
+        
+        Long id = genreFacade.create(genreFormBean);        
+        
+        //report success
+        redirectAttributes.addFlashAttribute("alert_success", "Genre with id " + id + " created");
+        return "redirect:" + uriComponentsBuilder.path("/genre").build().encode().toUriString();
     }
 }
