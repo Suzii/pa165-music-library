798962c2a04c8fce86910bcad83283712beb7ccc
Suzii <z.dankovcikova@gmail.com>
2015-12-13 21:31:27 +0100

    MusicianFacade exception handling

--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/MusicianFacadeImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/MusicianFacadeImpl.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165.musiclib.facade;
 
 import cz.muni.fi.pa165.musiclib.dto.MusicianDTO;
 import cz.muni.fi.pa165.musiclib.entity.Musician;
+import cz.muni.fi.pa165.musiclib.exception.NoSuchEntityFoundException;
 import cz.muni.fi.pa165.musiclib.service.BeanMappingService;
 import cz.muni.fi.pa165.musiclib.service.MusicianService;
 import java.util.List;
@@ -12,31 +13,39 @@ import org.springframework.stereotype.Service;
 /**
  * @author Milan
  */
-
 @Service
 @Transactional
 public class MusicianFacadeImpl implements MusicianFacade {
 
-    @Inject 
+    @Inject
     private BeanMappingService beanMappingService;
-    
+
     @Inject
     private MusicianService musicianService;
-    
+
     @Override
     public Long createMusician(MusicianDTO musician) {
+        if (musician == null) {
+            throw new IllegalArgumentException("musician cannot be null");
+        }
+
         musicianService.create(beanMappingService.mapTo(musician, Musician.class));
         return musician.getId();
     }
 
- /*   @Override
-    public void updateMusician(MusicianDTO musician) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
-*/
+    /*   @Override
+     public void updateMusician(MusicianDTO musician) {
+     throw new UnsupportedOperationException("Not supported yet."); 
+     }
+     */
     @Override
     public void removeMusician(Long musicianId) {
-        musicianService.remove(musicianService.findById(musicianId));
+        Musician musician = musicianService.findById(musicianId);
+        if (musician == null) {
+            throw new NoSuchEntityFoundException("No such musician exists");
+        }
+
+        musicianService.remove(musician);
     }
 
     @Override
@@ -46,12 +55,22 @@ public class MusicianFacadeImpl implements MusicianFacade {
 
     @Override
     public MusicianDTO getMusicianById(Long musicianId) {
-        return beanMappingService.mapTo(musicianService.findById(musicianId), MusicianDTO.class);
+        Musician musician = musicianService.findById(musicianId);
+        if (musician == null) {
+            throw new NoSuchEntityFoundException("No such musician exists");
+        }
+
+        return beanMappingService.mapTo(musician, MusicianDTO.class);
     }
 
     @Override
     public List<MusicianDTO> getMusicianByArtistName(String artistName) {
-        return beanMappingService.mapTo(musicianService.findByArtistName(artistName), MusicianDTO.class);
+        List<Musician> musicians = musicianService.findByArtistName(artistName);
+        if (musicians == null) {
+            throw new NoSuchEntityFoundException("No such musician exists");
+        }
+
+        return beanMappingService.mapTo(musicians, MusicianDTO.class);
     }
 
 }
