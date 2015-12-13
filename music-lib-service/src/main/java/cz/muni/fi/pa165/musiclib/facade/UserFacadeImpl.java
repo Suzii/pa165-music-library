c74718245b94124ee6ad5f1289f4343602b11334
Suzii <z.dankovcikova@gmail.com>
2015-12-13 21:31:55 +0100

    UserFacade exception handling

--- music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/UserFacadeImpl.java
+++ music-lib-service/src/main/java/cz/muni/fi/pa165/musiclib/facade/UserFacadeImpl.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.musiclib.facade;
 import cz.muni.fi.pa165.musiclib.dto.UserAuthenticationDTO;
 import cz.muni.fi.pa165.musiclib.dto.UserDTO;
 import cz.muni.fi.pa165.musiclib.entity.User;
+import cz.muni.fi.pa165.musiclib.exception.NoSuchEntityFoundException;
 import cz.muni.fi.pa165.musiclib.service.BeanMappingService;
 import cz.muni.fi.pa165.musiclib.service.UserService;
 import org.springframework.stereotype.Service;
@@ -28,32 +29,56 @@ public class UserFacadeImpl implements UserFacade {
 
     @Override
     public void registerUser(UserDTO userDTO, String encryptedPass) {
+        if (userDTO == null) {
+            throw new IllegalArgumentException("userDTO cannot be null");
+        }
+
         User user = beanMappingService.mapTo(userDTO, User.class);
         userService.registerUser(user, encryptedPass);
         userDTO.setId(user.getId());
     }
 
     @Override
-    public boolean authenticate(UserAuthenticationDTO u) {
-        return userService.authenticate(
-            userService.findUserById(u.getUserId()), u.getPassword());
+    public boolean authenticate(UserAuthenticationDTO userAuthenticateDTO) {
+        if (userAuthenticateDTO == null) {
+            throw new IllegalArgumentException("userAuthenticateDTO cannot be null");
+        }
+
+        User user = userService.findUserById(userAuthenticateDTO.getUserId());
+        if (user == null) {
+            throw new NoSuchEntityFoundException("No such user exists");
+        }
+
+        return userService.authenticate(user, userAuthenticateDTO.getPassword());
     }
 
     @Override
     public boolean isAdmin(UserDTO userDTO) {
+        if (userDTO == null) {
+            throw new IllegalArgumentException("userDTO cannot be null");
+        }
+
         return userService.isAdmin(beanMappingService.mapTo(userDTO, User.class));
     }
 
     @Override
     public UserDTO findUserById(Long userId) {
         User user = userService.findUserById(userId);
-        return (user == null) ? null : beanMappingService.mapTo(user, UserDTO.class);
+        if (user == null) {
+            throw new NoSuchEntityFoundException("No such user exists");
+        }
+
+        return beanMappingService.mapTo(user, UserDTO.class);
     }
 
     @Override
     public UserDTO findUserByEmail(String email) {
         User user = userService.findUserByEmail(email);
-        return (user == null) ? null : beanMappingService.mapTo(user, UserDTO.class);
+        if (user == null) {
+            throw new NoSuchEntityFoundException("No such user exists");
+        }
+
+        return beanMappingService.mapTo(user, UserDTO.class);
     }
 
     @Override
